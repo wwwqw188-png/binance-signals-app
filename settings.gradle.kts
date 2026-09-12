44aa277d64ca101f32c68efe9c6f77958c935e5d
wwwqw188-png <www.qw188@gmail.com>
2026-09-12 19:22:42 +0300

    Initialize settings for BinanceSignals project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+: pluginManagement. repositories. google. mavenCentral. gradlePluginPortal dependencyResolutionManagement. repositories. google. mavenCentral. rootProject.name = "BinanceSignals" include(" :app ")
